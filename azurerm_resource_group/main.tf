afb75adfcd45ea797848fa86482174f820f6ad9e
Siddhant Ramteke <siddhantramteke2000@gmail.com>
2026-06-16 01:24:56 +0530

    Hardcoded values

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  for_each = var.rg
-  name     = each.value.name
-  location = each.value.location
+  # for_each = var.rg
+  name     = "siddhant"
+  location = "westus"
 }
